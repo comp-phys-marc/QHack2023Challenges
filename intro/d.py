decfb0c02a18ef1a087132e44e4d8905c69dcf67
Marcus Edwards <edwardsmarc968@gmail.com>
2023-02-16 11:33:02 -0800

    Adopt naming convention

--- intro/d.py
+++ intro/d.py
@@ -2,9 +2,8 @@ import json
 import pennylane as qml
 import pennylane.numpy as np
 
-
 def is_product(state, subsystem, wires):
-    """Determines if a pure quantum state can be written as a product state between
+    """Determines if a pure quantum state can be written as a product state between 
     a subsystem of wires and their compliment.
 
     Args:
@@ -15,16 +14,62 @@ def is_product(state, subsystem, wires):
     Returns:
         (str): "yes" if the state is a product state or "no" if it isn't.
     """
-    subsystem_c = []
-    for wire in wires:
-        if wire not in subsystem:
-            subsystem_c.append(wire)
 
-    # to do...
-
-    # Put your solution here #
-    return
-
+    print("STATE:")
+    print(state)
+    print("SUBSYSTEMS:")
+    print(subsystem)
+    print("WIRES:")
+    print(wires)
+    
+    def is_diagonal(state):
+        diag_total = 0
+        for i in range(len(state[0])):
+            diag_total += abs(state[i][i]) ** 2
+            
+        n = 1 / np.sqrt(diag_total)
+        
+        diag_total = 0
+        for i in range(len(state[0])):
+            diag_total += state[i][i] * n
+    
+        if not diag_total == 1:
+            return False
+        
+        for i in range(len(state[0])):
+            for j in range(len(state[0])):
+                if i != j:
+                    if not state[i][j] == 0:
+                        return False
+                    if not state[j][i] == 0:
+                        return False
+        return True
+    
+    # make sure the state is pure
+    dagger = np.transpose(np.atleast_2d(state)).conj()
+    print("COMPLEX CONJUGAT TRANSPOSE:")
+    print(dagger)
+    
+    density_matrix = np.matmul(dagger, np.atleast_2d(state))
+    print("DENSITY MATRIX:")
+    print(density_matrix)
+    
+    if not is_diagonal(density_matrix):
+        return "no"
+    
+    # attempt Schmidt decomposition
+    try:
+        U, coeffs, V_dagger = np.linalg.svd(density_matrix)
+    except Exception as e:
+        print(f"svd impossible: {e}")
+        return "no"
+    
+    print("DECOMPOSITION:")
+    print(U)
+    print(coeffs)
+    print(V_dagger)
+    
+    return "yes"
 
 # These functions are responsible for testing the solution.
 def run(test_case_input: str) -> str:
@@ -34,15 +79,10 @@ def run(test_case_input: str) -> str:
     output = is_product(state, subsystem, wires)
     return output
 
-
 def check(solution_output: str, expected_output: str) -> None:
     assert solution_output == expected_output
 
-
-test_cases = [
-    ["[[0.707107, 0, 0, 0.707107], [0], [0, 1]]", "no"],
-    ["[[1, 0, 0, 0], [0], [0, 1]]", "yes"],
-]
+test_cases = [['[[0.707107, 0, 0, 0.707107], [0], [0, 1]]', 'no'], ['[[1, 0, 0, 0], [0], [0, 1]]', 'yes']]
 
 for i, (input_, expected_output) in enumerate(test_cases):
     print(f"Running test case {i} with input '{input_}'...")
